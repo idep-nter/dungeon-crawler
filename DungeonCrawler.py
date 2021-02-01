003c8c41179f03850a5b527b063d0f259bb1ae2e
Michael Sterzinger <69076772+idep-nter@users.noreply.github.com>
2021-02-01 16:40:01 +0100

    Update DungeonCrawler.py

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -10,14 +10,20 @@ class Creature:
 
     def attack(self, target): 
         attack = random.choice(range(self.dps[0], self.dps[1]+1))
-        if random.random() < self.critChance:
+        if random.random() < self.evasion:
+            attack = 0
+        elif random.random() < self.critChance:
             attack = (attack / 100) * 150
+            if target.shield:
+                attack = (attack / 100) * 70
+        elif target.shield:
+            attack = (attack / 100) * 70
         target.health -= attack
 
 class Player(Creature):
     def __init__(self, maxHealth=100, currentHealth=100, dps=[0, 0], 
         armorValue=0, evasion=0,critChance=0.01, maxWeight=100, currentWeight=0, 
-        weapon=None, armor=None, ring=None):
+        weapon=None, shield=None, armor=None, ring=None):
         super().__init__(name, dps, armorValue, evasion, critChance)
         self.maxHealth = maxHealth 
         self.currentHealth = currentHealth
@@ -25,6 +31,7 @@ class Player(Creature):
         self.currentWeight = currentWeight
         self.inventory = []
         self.weapon = weapon
+        self.shield = shield
         self.armor = armor
         self.ring = ring
 
@@ -38,11 +45,57 @@ class Player(Creature):
 
     def equipItem(self, item):
         if item == Weapon:
+            if self.weapon:
+                unEquipItem(self, item)
             self.weapon = item
+            self.weight += item.weight
+            self.dps += item.dps
+            self.critChance += item.critChance
+        elif item == Shield:
+            if self.shield:
+                unEquipItem(self, item)
+            self.shield = item
+            self.weight += item.weight
+            self.armorValue += item.armorValue
         elif item == Armor:
+            if self.armor:
+                unEquipItem(self, item)
             self.armor = item
+            self.weight += item.weight
+            self.armorValue += item.armorValue
+            self.evasion += item.evasion
         elif item == Ring:
+            if self.ring:
+                unEquipItem(self, item)
             self.ring = item
+            self.maxHealth += item.maxHealth
+            self.dps += item.dps
+            self.armorValue += item.armorValue
+            self.evasion += item.evasion
+            self.critChance += item.critChance
+
+    def unEquipItem(self, item):
+        if item == Weapon:
+            self.weapon = None
+            self.weight -= item.weight
+            self.dps -= item.dps
+            self.critChance -= item.critChance
+        elif item == Shield:
+            self.shield = None
+            self.weight -= item.weight
+            self.armorValue -= item.armorValue
+        elif item == Armor:
+            self.armor = None
+            self.weight -= item.weight
+            self.armorValue -= item.armorValue
+            self.evasion -= item.evasion
+        elif item == Ring:
+            self.ring = None
+            self.maxHealth -= item.maxHealth
+            self.dps -= item.dps
+            self.armorValue -= item.armorValue
+            self.evasion -= item.evasion
+            self.critChance -= item.critChance
 
     def drinkPotion(self, potion):
         self.health += potion.health
@@ -74,6 +127,11 @@ class Weapon(Item):
         self.dps = dps
         self.critChance = critChance
 
+class Shield(Item):
+    def __init__(self, value, weight, armorValue):
+        super().__init__(value, weight)
+        self.armorValue = armorValue
+
 class Armor(Item):
     def __init__(self, value, weight, armorValue, evasion):
         super().__init__(value, weight)
@@ -81,7 +139,7 @@ class Armor(Item):
         self.evasion = evasion
 
 class Ring(Item):
-    def __init__(self, value, weight=0.1, maxHealth=None, dps=None, 
+    def __init__(self, value, maxHealth=None, dps=None, 
         armorValue=None, evasion=None, critChance=None):
         super().__init__(value, weight)
         self.maxHealth = maxHealth
@@ -91,6 +149,6 @@ class Ring(Item):
         self.critChance = critChance
 
 class Potion(Item):
-    def __init__(self, value, health)
-    super().__init__(value, weight)
-    self.health = health
+    def __init__(self, value, health):
+        super().__init__(value, weight)
+        self.health = health
