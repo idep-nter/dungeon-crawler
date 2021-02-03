848847f05e1f79eb28a14400201c20dae4cdfc1a
Michael Sterzinger <69076772+idep-nter@users.noreply.github.com>
2021-02-03 17:16:46 +0100

    Update DungeonCrawler.py

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -1,5 +1,19 @@
 import random
 
+rustyDagger = Weapon('Rusty Dagger', 'common', 1, 2, '1hand', [2, 5], 0.1)
+longSwordOfMight = Weapon('Long sword of might', 'uncommon', 12, 5, '1hand', 
+    [10, 14], 0.05)
+ultraGreatswordOfDeath = Weapon('Ultra greatsword of death', 'rare', 50, 10, 
+    '2hand', [20, 30], 0.05)
+
+items = {'common' : {'weapon' : [rustyDagger], 'armor' : [], 'shield' : []},
+         'uncommon' : {'weapon' : [longSwordOfMight], 'armor' : [], 
+                       'shield' : []},
+         'rare' : {'weapon' : [ultraGreatswordOfDeath], 'armor' : [], 
+                   'shield' : []}}
+
+l1Map = {}
+
 class Creature:
     def __init__(self, name, health, dps, armorValue, evasion, critChance):
         self.health = health
@@ -19,10 +33,36 @@ class Creature:
             attack = (attack / 100) * 70
         target.health -= attack
 
+    def itemDrop(self, items):
+        if self == Boss:
+            item = random.choice(items['rare'])
+            Player.inventory.append(item)
+            #global items
+            items.remove[item]
+
+        n = random.random()
+        if n < 0.1:
+            item = random.choice(items['rare'])
+        elif n < 0.3:
+            item = random.choice(items['uncommon'])
+        else:
+            item = random.choice(items['common'])
+        item = random.choice(self.inventory)
+        Player.inventory.append(item)
+        #global items
+        items.remove[item]
+
+    def dropGold(self):
+        if self == Boss:
+            gold = random.randint(50, 100)
+        else:
+            gold = random.randint(5, 20)
+        Player.gold += gold
+
 class Player(Creature):
     def __init__(self, maxHealth=100, currentHealth=100, dps=[0, 0], 
         armorValue=0, evasion=0,critChance=0.01, maxWeight=100, currentWeight=0, 
-        weapon=None, shield=None, armor=None, ring=None):
+        weapon=None, shield=None, armor=None, ring=None, gold=0):
         super().__init__(name, dps, armorValue, evasion, critChance)
         self.maxHealth = maxHealth 
         self.currentHealth = currentHealth
@@ -33,6 +73,7 @@ class Player(Creature):
         self.shield = shield
         self.armor = armor
         self.ring = ring
+        self.gold = gold
 
     def showChar(self):
         attrs = vars(Player)
@@ -135,40 +176,43 @@ class Player(Creature):
 class Monster(Creature):
     def __init__(self, name, health, dps, armorValue, evasion, critChance):
         super().__init__(name, health, dps, armorValue, evasion, critChance)
-        self.inventory = []
 
-    def itemDrop(self):
-        item = random.choice(self.inventory)
-        Player.inventory.append(item)
+
+class Boss(Creature):
+    def __init__(self, name, health, dps, armorValue, evasion, critChance):
+        super().__init__(name, health, dps, armorValue, evasion, critChance)
+
+    #def specialAbility(self):
 
 class Item:
-    def __init__(self, rarity, value, weight):
+    def __init__(name, self, rarity, value, weight):
+        self.name = name
         self.rarity = rarity
         self.value = value
         self.weight = weight
 
 class Weapon(Item):
-    def __init__(self, rarity, value, weight, hand, dps, critChance):
-        super().__init__(rarity, value, weight)
+    def __init__(self, name, rarity, value, weight, hand, dps, critChance):
+        super().__init__(name, rarity, value, weight)
         self.hand = hand
         self.dps = dps
         self.critChance = critChance
 
 class Shield(Item):
-    def __init__(self, rarity, value, weight, armorValue):
-        super().__init__(rarity, value, weight)
+    def __init__(self, name, rarity, value, weight, armorValue):
+        super().__init__(name, rarity, value, weight)
         self.armorValue = armorValue
 
 class Armor(Item):
-    def __init__(self, rarity, value, weight, armorValue, evasion):
-        super().__init__(rarity, value, weight)
+    def __init__(self, name, rarity, value, weight, armorValue, evasion):
+        super().__init__(name, rarity, value, weight)
         self.armorValue = armorValue
         self.evasion = evasion
 
 class Ring(Item):
-    def __init__(self, rarity, value, maxHealth=None, dps=None, 
+    def __init__(self, name, rarity, value, maxHealth=None, dps=None, 
         armorValue=None, evasion=None, critChance=None):
-        super().__init__(rarity, value, weight)
+        super().__init__(name, rarity, value, weight)
         self.maxHealth = maxHealth
         self.dps = dps
         self.armorValue = armorValue
@@ -176,19 +220,31 @@ class Ring(Item):
         self.critChance = critChance
 
 class Potion(Item):
-    def __init__(self, rarity, value, health):
-        super().__init__(rarity, value, weight)
+    def __init__(self, name, rarity, value, health):
+        super().__init__(name, rarity, value, weight)
         self.health = health
-        
+
 class Chest():
     def __init__(self, items):
         self.items = items
 
+    def open(self):
+        itemFind(self, items)
+        trap(self)
+
     def itemFind(self, items):
-        item = random.choice(items)
+        n = random.random()
+        if n < 0.3:
+            item = random.choice(items['rare'])
+        elif n < 0.5:
+            item = random.choice(items['uncommon'])
+        else:
+            item = random.choice(items['common'])
+        item = random.choice(self.inventory)
         Player.inventory.append(item)
+        #global items
+        items.remove[item]
 
     def trap(self):
         if random.random() < 0.1:
-            Player.currentHealth -= 30
-
+            Player.currentHealth -= random.randint(10, 30)
