e1be0a54489fd61177b49e0d1fa9e7c87da4816e
awake <idep.nter@gmail.com>
2021-02-06 12:29:51 +0100

    Wohhoooooo

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -16,7 +16,7 @@ class Creature:
             return False
         if random.random() < self.critChance:
             attack = (attack / 100) * 150
-        attack = attack / (1 + (armorValue / 100))
+        attack = attack / (1 + (target.armorValue / 100))
         if target.shield:
             attack = (attack / 100) * 70
         target.health -= attack
@@ -47,7 +47,7 @@ class Creature:
         Player.gold += gold
 
 class Player(Creature):
-    def __init__(self, maxHealth=100, currentHealth=100, dps=[1,4],
+    def __init__(self, name, maxHealth=100, currentHealth=100, dps=[1,4],
                  armorValue=0, evasion=0.2, critChance=0.01, maxWeight=100,
                  currentWeight=0, weapon=None, shield=None, armor=None,
                  ring=None, gold=0):
@@ -76,32 +76,32 @@ class Player(Creature):
             if item not in self.inventory:
                 raise ValueError                  
             if item == Weapon:
-                if not handCheck(self, item):
+                if not self.handCheck(item):
                     raise ValueError
                 if self.weapon:
-                    unequipItem(self, item)
+                    self.unequipItem(item)
                 self.weapon = item
                 self.weight += item.weight
                 self.dps += item.dps
                 self.critChance += item.critChance
             elif item == Shield:
-                if not handCheck(self, item):
+                if not self.handCheck(item):
                     raise ValueError
                 if self.shield:
-                    unequipItem(self, item)
+                    self.unequipItem(item)
                 self.shield = item
                 self.weight += item.weight
                 self.armorValue += item.armorValue
             elif item == Armor:
                 if self.armor:
-                    unequipItem(self, item)
+                    self.unequipItem(item)
                 self.armor = item
                 self.weight += item.weight
                 self.armorValue += item.armorValue
                 self.evasion += item.evasion
             elif item == Ring:
                 if self.ring:
-                    unequipItem(self, item)
+                    self.unequipItem(item)
                 self.ring = item
                 self.maxHealth += item.maxHealth
                 self.dps += item.dps
@@ -149,8 +149,8 @@ class Player(Creature):
                 raise ValueError
             self.inventory.remove(potion)
             self.health += potion.health
-            if self.health > maxHealth:
-                self.health = maxHealth
+            if self.health > self.maxHealth:
+                self.health = self.maxHealth
 
         except ValueError:
             print(f'{potion} not in the inventory!')
@@ -202,7 +202,7 @@ class Armor(Item):
 class Ring(Item):
     def __init__(self, name, rarity, value, maxHealth=None, dps=None, 
         armorValue=None, evasion=None, critChance=None):
-        super().__init__(name, rarity, value, weight)
+        super().__init__(name, rarity, value)
         self.maxHealth = maxHealth
         self.dps = dps
         self.armorValue = armorValue
@@ -211,7 +211,7 @@ class Ring(Item):
 
 class Potion(Item):
     def __init__(self, name, rarity, value, health):
-        super().__init__(name, rarity, value, weight)
+        super().__init__(name, rarity, value)
         self.health = health
 
 class Chest():
@@ -219,8 +219,8 @@ class Chest():
         self.items = items
 
     def open(self):
-        itemFind(self, items)
-        trap(self)
+        self.itemFind(items)
+        self.trap()
 
     def itemFind(self, items):
         n = random.random()
