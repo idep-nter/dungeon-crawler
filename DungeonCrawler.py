81dcf9fb62d3cd895af3a510ee621bdd7370f2db
awake <idep.nter@gmail.com>
2021-02-06 14:11:47 +0100

    Wohhoooooo

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -10,8 +10,8 @@ class Creature:
         self.evasion = evasion
         self.critChance = critChance
 
-    def attack(self, target): 
-        attack = random.choice(range(self.dps[0], self.dps[1]+1))
+    def attack(self, target):
+        attack = random.choice(range(self.dps[0], self.dps[1] + 1))
         if random.random() < target.evasion:
             return False
         if random.random() < self.critChance:
@@ -25,7 +25,7 @@ class Creature:
         if self == Boss:
             item = random.choice(items['rare'])
             Player.inventory.append(item)
-            #global items
+            # global items
             items.remove[item]
 
         n = random.random()
@@ -36,7 +36,7 @@ class Creature:
         else:
             item = random.choice(items['common'])
         Player.inventory.append(item)
-        #global items
+        # global items
         items.remove[item]
 
     def dropGold(self):
@@ -46,13 +46,14 @@ class Creature:
             gold = random.randint(5, 20)
         Player.gold += gold
 
+
 class Player(Creature):
-    def __init__(self, name, maxHealth=100, currentHealth=100, dps=[1,4],
+    def __init__(self, name, maxHealth=100, currentHealth=100, dps=[1, 4],
                  armorValue=0, evasion=0.2, critChance=0.01, maxWeight=100,
                  currentWeight=0, weapon=None, shield=None, armor=None,
                  ring=None, gold=0):
         super().__init__(name, dps, armorValue, evasion, critChance)
-        self.maxHealth = maxHealth 
+        self.maxHealth = maxHealth
         self.currentHealth = currentHealth
         self.maxWeight = maxWeight
         self.currentWeight = currentWeight
@@ -74,7 +75,7 @@ class Player(Creature):
     def equipItem(self, item):
         try:
             if item not in self.inventory:
-                raise ValueError                  
+                raise ValueError
             if item == Weapon:
                 if not self.handCheck(item):
                     raise ValueError
@@ -111,7 +112,7 @@ class Player(Creature):
             self.inventory.remove(item)
 
         except ValueError:
-            print(f'{item} not in the inventory or check hands!') #change
+            print(f'{item} not in the inventory or check hands!')  # change
 
     def unequipItem(self, item):
         try:
@@ -138,7 +139,7 @@ class Player(Creature):
                 self.armorValue -= item.armorValue
                 self.evasion -= item.evasion
                 self.critChance -= item.critChance
-            self.inventory.append(item) 
+            self.inventory.append(item)
 
         except ValueError:
             print(f'{item} not in the inventory!')
@@ -161,6 +162,7 @@ class Player(Creature):
         if item == Shield:
             return False if self.weapon.hand == 'two hand' else True
 
+
 class Monster(Creature):
     def __init__(self, name, health, dps, armorValue, evasion, critChance):
         super().__init__(name, health, dps, armorValue, evasion, critChance)
@@ -170,7 +172,8 @@ class Boss(Creature):
     def __init__(self, name, health, dps, armorValue, evasion, critChance):
         super().__init__(name, health, dps, armorValue, evasion, critChance)
 
-    #def specialAbility(self):
+    # def specialAbility(self):
+
 
 class Item:
     def __init__(self, name, type, rarity, value, weight):
@@ -180,6 +183,7 @@ class Item:
         self.value = value
         self.weight = weight
 
+
 class Weapon(Item):
     def __init__(self, name, type, rarity, value, weight, hand, dps,
                  critChance):
@@ -188,10 +192,12 @@ class Weapon(Item):
         self.dps = dps
         self.critChance = critChance
 
+
 class Shield(Item):
     def __init__(self, name, type, rarity, value, weight, armorValue):
         super().__init__(name, type, rarity, value, weight)
         self.armorValue = armorValue
+
 
 class Armor(Item):
     def __init__(self, name, type, rarity, value, weight, armorValue, evasion):
@@ -199,9 +205,10 @@ class Armor(Item):
         self.armorValue = armorValue
         self.evasion = evasion
 
+
 class Ring(Item):
-    def __init__(self, name, rarity, value, maxHealth=None, dps=None, 
-        armorValue=None, evasion=None, critChance=None):
+    def __init__(self, name, rarity, value, maxHealth=None, dps=None,
+                 armorValue=None, evasion=None, critChance=None):
         super().__init__(name, rarity, value)
         self.maxHealth = maxHealth
         self.dps = dps
@@ -209,12 +216,14 @@ class Ring(Item):
         self.evasion = evasion
         self.critChance = critChance
 
+
 class Potion(Item):
     def __init__(self, name, rarity, value, health):
         super().__init__(name, rarity, value)
         self.health = health
 
-class Chest():
+
+class Chest:
     def __init__(self, items):
         self.items = items
 
@@ -231,42 +240,43 @@ class Chest():
         else:
             item = random.choice(items['common'])
         Player.inventory.append(item)
-        #global items
+        # global items
         items.remove[item]
 
     def trap(self):
         if random.random() < 0.1:
             Player.currentHealth -= random.randint(10, 30)
 
-dagger = Weapon('Dagger', 'dagger' 'common', 3, 2, 'one hand', [3,7], 0.2)
-axe = Weapon('Axe', 'axe' 'common', 5, 5, 'one hand', [5,10], 0.1)
-longSword = Weapon('Long Sword', 'long sword' 'common', 5, 4, 'one hand', [7,9],
-                   0.05)
+
+dagger = Weapon('Dagger', 'dagger' 'common', 3, 2, 'one hand', [3, 7], 0.2)
+axe = Weapon('Axe', 'axe' 'common', 5, 5, 'one hand', [5, 10], 0.1)
+longSword = Weapon('Long Sword', 'long sword' 'common', 5, 4, 'one hand',
+                   [7, 9], 0.05)
 twoHandedSword = Weapon('Two Handed Sword', 'two handed sword' 'common', 8, 8,
-                        'two hand', [11,15], 0.1)
+                        'two hand', [11, 15], 0.1)
 twoHandedAxe = Weapon('Two Handed Axe', 'two handed axe' 'common', 9, 10,
-                        'two hand', [10,17], 0.1)
+                      'two hand', [10, 17], 0.1)
 shadowStrike = Weapon('Shadow Strike', 'dagger', 'uncommon', 23, 2, 'one hand',
-                      [12,16], 0.2)
-peaceMaker = Weapon('Peace Maker', 'axe', 'uncommon', 28, 6, 'one hand', [12,20],
-                    0.1)
+                      [12, 16], 0.2)
+peaceMaker = Weapon('Peace Maker', 'axe', 'uncommon', 28, 6, 'one hand',
+                    [12, 20], 0.1)
 oathKeeper = Weapon('Oath Keeper', 'long sword', 'uncommon', 26, 4, 'one hand',
-                    [15,18], 0.05)
+                    [15, 18], 0.05)
 soulReaper = Weapon('Soul Reaper', 'two handed sword' 'uncommon', 35, 10,
-                        'two hand', [35,40], 0.1)
+                    'two hand', [35, 40], 0.1)
 rapture = Weapon('Rapture', 'two handed axe' 'uncommon', 37, 11,
-                        'two hand', [31,44], 0.1)
+                 'two hand', [31, 44], 0.1)
 sinisterCarver = Weapon('Sinister Carver', 'dagger' 'rare', 50, 2, 'one hand',
                         [42, 50], 0.2)
-harbinger = Weapon('Harbinger', 'axe' 'rare', 56, 6, 'one hand', [45,60], 0.1)
+harbinger = Weapon('Harbinger', 'axe' 'rare', 56, 6, 'one hand', [45, 60], 0.1)
 blindJustice = Weapon('Blind Justice', 'long sword' 'rare', 58, 4, 'one hand',
-                      [51,55], 0.05)
+                      [51, 55], 0.05)
 stormbringer = Weapon('Two Handed Sword', 'two handed sword' 'rare', 72, 12,
-                        'two hand', [72,79], 0.1)
-eclipse = Weapon('Eclipse', 'two handed axe' 'rare', 78, 12, 'two hand', [68,85],
+                      'two hand', [72, 79], 0.1)
+eclipse = Weapon('Eclipse', 'two handed axe' 'rare', 78, 12, 'two hand', [68, 85],
                  0.1)
 
-#add more difference between shield types
+# add more difference between shield types
 smallShield = Shield('Small Shield', 'small shield', 'common', 4, 5, 5)
 greatShield = Shield('Great Shield', 'great shield', 'common', 6, 10, 11)
 dawnGuard = Shield('Dawn Guard', 'small shield', 'uncommon', 13, 6, 13)
